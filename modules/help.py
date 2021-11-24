f27d8c913e8d4e6b45874227b9459283b10f73ce
Lartza <Lartza@users.noreply.github.com>
2021-11-24 14:20:10 +0200

    Bad copypasting

--- modules/help.py
+++ modules/help.py
@@ -6,7 +6,7 @@ component = tanjun.Component()
 @component.with_slash_command
 @tanjun.as_slash_command('help', 'Link to the documentation')
 async def help(ctx: tanjun.abc.SlashContext) -> None:
-    await ctx.respond('https://github.com/Lartza/GalatiteBot2/SIRA-Bot#commands')
+    await ctx.respond('https://github.com/Lartza/GalatiteBot2#commands')
 
 
 loader = component.make_loader()
