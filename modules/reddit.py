3bbd840417a15f3cea0f5c52ecb475d665033271
Lartza <Lartza@users.noreply.github.com>
2021-11-30 19:52:05 +0200

    Fix admin roles example and reddit command

--- modules/reddit.py
+++ modules/reddit.py
@@ -9,10 +9,10 @@ component = tanjun.Component()
 
 @component.with_slash_command
 @tanjun.with_check(role_check)
-@tanjun.as_slash_command('reddit', 'Admin only test command')
+@tanjun.as_slash_command('reddit', 'Admin only, force top reddit post job')
 async def reddit(ctx: tanjun.abc.SlashContext) -> None:
+    await ctx.create_initial_response('Posting!', flags=hikari.MessageFlag.EPHEMERAL)
     await jobs.reddit.run(ctx.rest)
-    await ctx.create_initial_response('Posted or errored!', flags=hikari.MessageFlag.EPHEMERAL)
 
 
 loader = component.make_loader()
