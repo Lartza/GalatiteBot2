8ff52b0f4e17047bf438c4aa6c24c2ea50b5067b
Lartza <Lartza@users.noreply.github.com>
2024-11-03 23:36:48 +0200

    Add auth to reddit job

--- jobs/reddit.py
+++ jobs/reddit.py
@@ -1,3 +1,5 @@
+import os
+import base64
 import hikari
 import tanjun
 import aiohttp
@@ -7,49 +9,89 @@ import config
 
 component = tanjun.Component()
 
+app_id = os.getenv("REDDIT_APP_ID")
+app_secret = os.getenv("REDDIT_APP_SECRET")
+app_username = os.getenv("REDDIT_APP_USERNAME")
+
+TOKEN_URL = "https://www.reddit.com/api/v1/access_token"
+
 
 @component.with_schedule
 @tanjun.as_time_schedule(hours=16, minutes=00)
 async def run(client: alluka.Injected[tanjun.Client]) -> None:
+    if not app_id or not app_secret or not app_username:
+        return
+
+    auth = base64.b64encode(f"{app_id}:{app_secret}".encode()).decode()
+
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.reddit.com/r/elderscrollsonline/top/.json?t=day') as feed_response:
-            feed_json = await feed_response.json()
-            link = feed_json['data']['children'][0]['data']['permalink']
+        async with session.post(
+            TOKEN_URL,
+            data={"grant_type": "client_credentials"},
+            headers={
+                "Authorization": f"Basic {auth}",
+                "User-Agent": f"GalatiteBot2 (by /u/{app_username})",
+            },
+        ) as token_response:
+            if token_response.status != 200:
+                print(f"Failed to obtain token: {token_response.status}")
+                return
+            token_data = await token_response.json()
+            access_token = token_data.get("access_token")
 
-            async with session.get('https://www.reddit.com' + link + '.json') as response:
-                json = await response.json()
-                data = json[0]['data']['children'][0]['data']
+        if access_token:
+            async with session.get(
+                "https://oauth.reddit.com/r/elderscrollsonline/top/.json?t=day",
+                headers={
+                    "Authorization": f"bearer {access_token}",
+                    "User-Agent": f"GalatiteBot2 (by /u/{app_username})",
+                },
+            ) as feed_response:
+                if feed_response.status != 200:
+                    print(f"Failed to fetch feed: {feed_response.status}")
+                    return
+                feed_json = await feed_response.json()
+                link = feed_json["data"]["children"][0]["data"]["permalink"]
 
-                if data['is_video']:
-                    embed = (
-                        hikari.Embed(
-                            colour=hikari.Colour(0x0099ff),
-                            title=data['title'],
-                            url='https://www.reddit.com' + data['permalink'],
-                            description='Videos not supported :(',
+                async with session.get(
+                    "https://oauth.reddit.com" + link + ".json",
+                    headers={
+                        "Authorization": f"bearer {access_token}",
+                        "User-Agent": f"GalatiteBot2 (by /u/{app_username})",
+                    },
+                ) as response:
+                    if response.status != 200:
+                        print(f"Failed to fetch post details: {response.status}")
+                        return
+                    json = await response.json()
+                    data = json[0]["data"]["children"][0]["data"]
+
+                    if data["is_video"]:
+                        embed = hikari.Embed(
+                            colour=hikari.Colour(0x0099FF),
+                            title=data["title"],
+                            url="https://www.reddit.com" + data["permalink"],
+                            description="Videos not supported :(",
                         )
-                    )
-                else:
-                    embed = (
-                        hikari.Embed(
-                            colour=hikari.Colour(0x0099ff),
-                            title=data['title'],
-                            url='https://www.reddit.com' + data['permalink'],
-                            description=data['selftext'],
+                    else:
+                        embed = hikari.Embed(
+                            colour=hikari.Colour(0x0099FF),
+                            title=data["title"],
+                            url="https://www.reddit.com" + data["permalink"],
+                            description=data.get("selftext", ""),
                         )
+
+                        try:
+                            embed.set_image(data["url_overridden_by_dest"])
+                        except KeyError:
+                            pass
+
+                    embed.set_author(name="u/" + data["author"])
+                    embed.set_footer(
+                        text="This is today's top post of /r/elderscrollsonline.",
                     )
 
-                    try:
-                        embed.set_image(data['url_overridden_by_dest'])
-                    except KeyError:
-                        pass
-
-                embed.set_author(name='u/' + data['author'])
-                embed.set_footer(
-                    text="This is today's top post of /r/elderscrollsonline.",
-                )
-
-                await client.rest.create_message(config.REDDIT_CHANNEL, embed)
+                    await client.rest.create_message(config.REDDIT_CHANNEL, embed)
 
 
 loader = component.make_loader()
--- modules/ping.py
+++ modules/ping.py
@@ -7,68 +7,88 @@ import tanjun
 component = tanjun.Component()
 
 
-async def ping_response(mention: str, rest: hikari.api.rest.RESTClient) ->\
-        Tuple[str, hikari.api.special_endpoints.MessageActionRowBuilder]:
+def get_hit_message(mention: str) -> str:
+    hit_rnd = random.randint(0, 24)
+    if hit_rnd == 0:
+        return (
+            f"-PONG! {mention} hit the ball so hard :muscle:, {mention}'s racket broke. "
+            f"But {mention} has a spare racket. \nThe game continues. :ping_pong:"
+        )
+    if hit_rnd == 1:
+        return (
+            f"-Plash!... {mention} just used a fish :fish: to hit the ball... Guess that counts. \n"
+            "The game continues! :ping_pong:"
+        )
+    if hit_rnd == 2:
+        return (
+            f"-Stop. **Rigurt the Brash** needs {mention}'s help. He ordered fitting Ping-Pong "
+            f"attire. Somehow it never arrived and he needs {mention} to investigate :mag:.\n"
+            f"{mention} starts on a quest to find out what happened. \n{mention} finds a "
+            "referee hiding something under the bench. It is a hat with a racket glued to it :tophat: "
+            f":ping_pong:.\n{mention} gives the hat to **Rigurt the Brash** and the game continues!"
+        )
+    if hit_rnd == 3:
+        return f"""-pong. {mention} hit the ball! :ping_pong:. \n
+        The ball suddenly turns red :red_circle:. \n
+        The ball says: "You probably don't recognize me, because of the red arm." \n
+        Everyone keeps playing as if nothing has happened. \nThe game continues. :ping_pong:"""
+    if hit_rnd == 4:
+        return (
+            f"-PONG!!!! {mention} gets angry and triggers his Super Saiyajin Mode "
+            f":man_supervillain:. \n{mention} almost breaks the table... but the game continues! "
+            ":ping_pong:"
+        )
+    return f"-pong. {mention} hit the ball! :ping_pong:"
+
+
+async def ping_response(
+    mention: str, rest: hikari.api.rest.RESTClient
+) -> Tuple[str, hikari.api.special_endpoints.MessageActionRowBuilder]:
     msg = None
     rnd = random.randint(0, 99)
 
     if rnd < 85:
-        hit_rnd = random.randint(0, 24)
-        if hit_rnd == 0:
-            msg = f"-PONG! {mention} hit the ball so hard :muscle:, {mention}'s racket broke. " \
-                  f"But {mention} has a spare racket. \nThe game continues. :ping_pong:"
-        elif hit_rnd == 1:
-            msg = f'-Plash!... {mention} just used a fish :fish: to hit the ball... Guess that counts. \n' \
-                  'The game continues! :ping_pong:'
-        elif hit_rnd == 2:
-            msg = f"-Stop. **Rigurt the Brash** needs {mention}'s help. He ordered fitting Ping-Pong " \
-                  f"attire. Somehow it never arrived and he needs {mention} to investigate :mag:.\n" \
-                  f"{mention} starts on a quest to find out what happened. \n{mention} finds a " \
-                  "referee hiding something under the bench. It is a hat with a racket glued to it :tophat: " \
-                  f":ping_pong:.\n{mention} gives the hat to **Rigurt the Brash** and the game continues!"
-        elif hit_rnd == 3:
-            msg = f'''-pong. {mention} hit the ball! :ping_pong:. \n
-            The ball suddenly turns red :red_circle:. \n
-            The ball says: "You probably don't recognize me, because of the red arm." \n
-            Everyone keeps playing as if nothing has happened. \nThe game continues. :ping_pong:'''
-        elif hit_rnd == 4:
-            msg = f'-PONG!!!! {mention} gets angry and triggers his Super Saiyajin Mode ' \
-                  f':man_supervillain:. \n{mention} almost breaks the table... but the game continues! ' \
-                  ':ping_pong:'
-        else:
-            msg = f'-pong. {mention} hit the ball! :ping_pong:'
+        msg = get_hit_message(mention)
     elif rnd < 90:
         win_rnd = random.randint(0, 2)
         if win_rnd == 0:
-            msg = '-pong! Dremora invade the earth and everyone was vaporized by Molag Bal :smiling_imp:. \nSince ' \
-                  f'{mention} was the last to hit the ball, {mention} wins by ' \
-                  f'vaporization-elimination! \nCongratulation to {mention}!!! :partying_face:'
+            msg = (
+                "-pong! Dremora invade the earth and everyone was vaporized by Molag Bal :smiling_imp:. \nSince "
+                f"{mention} was the last to hit the ball, {mention} wins by "
+                f"vaporization-elimination! \nCongratulation to {mention}!!! :partying_face:"
+            )
         elif win_rnd == 1:
-            msg = f'-Ohh no! {mention} missed. **Puff** It turns out, that {mention} was ' \
-                  'Sheogorath in disguise the WHOLE TIME! :clown: \nSince Sheo is threatening me with a spoon right ' \
-                  f'now: \n{mention} wins!!! :partying_face:'
+            msg = (
+                f"-Ohh no! {mention} missed. **Puff** It turns out, that {mention} was "
+                "Sheogorath in disguise the WHOLE TIME! :clown: \nSince Sheo is threatening me with a spoon right "
+                f"now: \n{mention} wins!!! :partying_face:"
+            )
         elif win_rnd == 2:
-            msg = '01000100011011110010000001111001011011110111010100100000011010110110111001101111011101110010000001' \
-                  '10100001101111011101110010000001101100011011110110111001100111001000000100100100100111011101100110' \
-                  '01010010000001100010011001010110010101101110001000000110110001101111011011110110101101101001011011' \
-                  '1001100111001000000110011001101111011100100010000001111001011011110111010100111111\n' \
-                  '...ohh sorry... Did not see you there... I mean: \n' \
-                  f'-Pong! {mention} wins! :partying_face:'
+            msg = (
+                "01000100011011110010000001111001011011110111010100100000011010110110111001101111011101110010000001"
+                "10100001101111011101110010000001101100011011110110111001100111001000000100100100100111011101100110"
+                "01010010000001100010011001010110010101101110001000000110110001101111011011110110101101101001011011"
+                "1001100111001000000110011001101111011100100010000001111001011011110111010100111111\n"
+                "...ohh sorry... Did not see you there... I mean: \n"
+                f"-Pong! {mention} wins! :partying_face:"
+            )
     elif rnd < 100:
         lose_rnd = random.randint(0, 9)
         if lose_rnd == 0:
-            msg = f'-Ohh no... {mention} missed. \nLooks like {mention} has taken too much moon' \
-                  f' sugar :woozy_face:. \n{mention} is out! :thumbsdown:'
+            msg = (
+                f"-Ohh no... {mention} missed. \nLooks like {mention} has taken too much moon"
+                f" sugar :woozy_face:. \n{mention} is out! :thumbsdown:"
+            )
         else:
-            msg = f'-Ohh no... {mention} missed. \n{mention} is out! :thumbsdown:'
+            msg = f"-Ohh no... {mention} missed. \n{mention} is out! :thumbsdown:"
 
     row = rest.build_message_action_row()
-    row.add_interactive_button(hikari.ButtonStyle.PRIMARY, '🏓', emoji='🏓')
+    row.add_interactive_button(hikari.ButtonStyle.PRIMARY, "🏓", emoji="🏓")
     return msg, row
 
 
 @component.with_slash_command
-@tanjun.as_slash_command('ping', 'Pong! (A silly little game)')
+@tanjun.as_slash_command("ping", "Pong! (A silly little game)")
 async def ping(ctx: tanjun.abc.Context) -> None:
     msg, row = await ping_response(ctx.author.mention, ctx.rest)
 
